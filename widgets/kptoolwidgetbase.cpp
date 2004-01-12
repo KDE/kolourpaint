3b47ec57028f1f9e92246d85e7e68646a9f06e5a
Clarence Dang <dang@kde.org>
2004-01-12 02:13:49 +0000

    CVS_SILENT no debug - not working on that till after 1.0
    svn path=/trunk/kdegraphics/kolourpaint/; revision=278973

--- widgets/kptoolwidgetbase.cpp
+++ widgets/kptoolwidgetbase.cpp
@@ -29,7 +29,7 @@
    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#define DEBUG_KP_TOOL_WIDGET_BASE 1
+#define DEBUG_KP_TOOL_WIDGET_BASE 0
 
 #include <qbitmap.h>
 #include <qcolor.h>
