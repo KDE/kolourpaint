14a2da591335cfe9c207667ea4a6e0e8d6c1e911
Clarence Dang <dang@kde.org>
2004-01-18 10:34:10 +0000

    Bound dragging out rectangular selections to the document rectangle (it wouldn't make sense selecting anything outside the document as it would  be undefined)
    svn path=/trunk/kdegraphics/kolourpaint/; revision=280701

--- tools/kptoolselection.cpp
+++ tools/kptoolselection.cpp
@@ -2,17 +2,17 @@
 /*
    Copyright (c) 2003-2004 Clarence Dang <dang@kde.org>
    All rights reserved.
-   
+
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
    are met:
-   
+
    1. Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
    2. Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-   
+
    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -174,8 +174,11 @@ void kpToolSelection::draw (const QPoint &thisPoint, const QPoint & /*lastPoint*
         switch (m_mode)
         {
         case kpToolSelection::Rectangle:
-            document ()->setSelection (kpSelection (kpSelection::Rectangle, normalizedRect));
+        {
+            const QRect usefulRect = normalizedRect.intersect (document ()->rect ());
+            document ()->setSelection (kpSelection (kpSelection::Rectangle, usefulRect));
             break;
+        }
         case kpToolSelection::Ellipse:
             document ()->setSelection (kpSelection (kpSelection::Ellipse, normalizedRect));
             break;
