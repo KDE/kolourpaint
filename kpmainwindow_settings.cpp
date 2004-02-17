b2128be4005566a37fa69ecff8bfb85f52e0c10d
Clarence Dang <dang@kde.org>
2004-02-17 07:56:55 +0000

    Disable "Configure KolourPaint..." for now - there isn't much to configure (Undo history length, # recent files) ATM and it should probably be redesigned to use KConfigXT anyway.  For post 1.0.
    svn path=/trunk/kdegraphics/kolourpaint/; revision=288646

--- kpmainwindow_settings.cpp
+++ kpmainwindow_settings.cpp
@@ -2,17 +2,17 @@
 /*
    Copyright (c) 2003-2004 Clarence Dang <dang@kde.org>
    All rights reserved.
-   
+
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
    are met:
-   
+
    1. Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
    2. Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-   
+
    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -35,7 +35,6 @@
 #include <klocale.h>
 #include <kstdaction.h>
 
-#include <kpconfigdialog.h>
 #include <kpdefs.h>
 #include <kpmainwindow.h>
 
@@ -60,8 +59,8 @@ void kpMainWindow::setupSettingsMenuActions ()
 
     m_actionKeyBindings = KStdAction::keyBindings (this, SLOT (slotKeyBindings ()), ac);
     m_actionConfigureToolbars = KStdAction::configureToolbars (this, SLOT (slotConfigureToolBars ()), ac);
-    m_actionConfigure = KStdAction::preferences (this, SLOT (slotConfigure ()), ac);
-    
+    // m_actionConfigure = KStdAction::preferences (this, SLOT (slotConfigure ()), ac);
+
     enableSettingsMenuDocumentActions (false);
 }
 
@@ -97,10 +96,7 @@ void kpMainWindow::slotActionShowPathToggled (bool on)
 // private slot
 void kpMainWindow::slotConfigure ()
 {
-    // TODO: parent
-    kpConfigDialog *dialog = new kpConfigDialog ();
-    dialog->exec ();
-    delete dialog;
+    // TODO
 }
 
 // private slot
