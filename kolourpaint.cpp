b5a8dc6e12052415802dc8f35dec4e0d97c12bec
Clarence Dang <dang@kde.org>
2004-06-04 11:44:59 +0000

    dbl bracketting in about dlg looks strange
    svn path=/trunk/kdegraphics/kolourpaint/; revision=317605

--- kolourpaint.cpp
+++ kolourpaint.cpp
@@ -60,7 +60,7 @@ int main (int argc, char *argv [])
     (
         "kolourpaint",
         I18N_NOOP("KolourPaint"),
-        "1.1 (unstable)",  // SYNC: with VERSION
+        "1.1 UNSTABLE",  // SYNC: with VERSION
         I18N_NOOP("Paint Program for KDE"),
         KAboutData::License_Custom,
         "Copyright (c) 2003-2004 Clarence Dang",
