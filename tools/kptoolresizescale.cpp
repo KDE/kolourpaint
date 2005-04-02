eb56bc8e4b99a6c3215feb398c0cdec6028f7f26
Stefan Winter <swinter@kde.org>
2005-04-02 17:40:09 +0000

    fixing XML markup (KBabel complains)
    svn path=/trunk/kdegraphics/kolourpaint/; revision=402675

--- tools/kptoolresizescale.cpp
+++ tools/kptoolresizescale.cpp
@@ -650,10 +650,10 @@ void kpToolResizeScaleDialog::createOperationGroupBox (QWidget *baseWidget)
                   " by creating new areas to the right and/or the bottom"
                   " (filled in with the background color) or"
                   " decreased by cutting"
-                  " it at the right and/or the bottom."
+                  " it at the right and/or the bottom.</li>"
 
                   "<li><b>Scale</b>: The picture will be expanded"
-                  " by duplicating pixels or squashed by dropping pixels."
+                  " by duplicating pixels or squashed by dropping pixels.</li>"
 
                   "<li><b>Smooth Scale</b>: This is the same as"
                   " <i>Scale</i> except that it blends neighbouring"
