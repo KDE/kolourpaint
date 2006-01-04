44761c6f5f7db4fc25d86f8facdfe34948c2b7b2
Clarence Dang <dang@kde.org>
2006-01-04 12:02:47 +0000

    more scripts
    svn path=/branches/kolourpaint/control/; revision=494240

---
+++ scripts/gen_mimetype_line.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Regenerates the "MimeType=" line in kolourpaint.desktop.
+#
+# Read kolourpaint.desktop for Read usage.
+# Read kpdocumentsaveoptions.cpp for Write usage.
+
+MODE="Read"
+[ "$1" ] && MODE="Write"
+
+echo Mode = $MODE > /dev/stderr
+
+echo -n MimeType=
+firstTime=1
+for g in $(for f in `grep -l ${MODE}=true *.kimgio`; do echo `grep Mimetype= $f`; done | cut -d= -f2 | sort | sed -e '/^$/d')
+do
+    if [ "$firstTime" ]
+    then
+        firstTime=
+    else
+        echo -n ';'
+    fi
+    echo -n $g
+done
+echo
