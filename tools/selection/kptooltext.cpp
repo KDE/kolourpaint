KDE/kolourpaint

commit 54f81003ad8c4cdc1573c6f57262e268868c239f

Split some code out of kpToolText::createMoreSelectionAndUpdateStatusBar() into shouldCreate()
svn path=/trunk/KDE/kdegraphics/kolourpaint/; revision=606478

tools/selection/kptooltext.cpp
@@ -229,76 +229,125 @@ QCursor kpToolText::cursorInsideSelection () const
 }
 
 
+// private
+bool kpToolText::shouldCreate (const kpTextStyle &textStyle,
+        int *minimumWidthOut, int *minimumHeightOut)
+{
+    if (m_dragHasBegun || m_currentPoint != m_startPoint)
+    {
+    #if DEBUG_KP_TOOL_TEXT && 1
+        kDebug () << "\tdrag creating text box" << endl;
+    #endif
+        *minimumWidthOut = kpSelection::minimumWidthForTextStyle (textStyle);
+        *minimumHeightOut = kpSelection::minimumHeightForTextStyle (textStyle);
+        return true/*do create text box*/;
+    }
+
+    
+    //
+    // User is possibly clicking to create a text box.
+    //
+    // Create a text box of reasonable ("preferred minimum") size.
+    //
+    // However, if it turns out that this is just the start of the drag,
+    // we will be called again but the above code will execute instead,
+    // ignoring this resonable size.
+    //
+
+#if DEBUG_KP_TOOL_TEXT && 1
+    kDebug () << "\tclick creating text box" << endl;
+#endif
+
+    // (Click creating text box with RMB would not be obvious
+    //  since RMB menu most likely hides text box immediately
+    //  afterwards)
+    if (m_mouseButton == 1)
+        return false/*do not create text box*/;
+
+
+    //
+    // Calculate Width.
+    //
+    
+    // Get reasonable width for a text box.
+    *minimumWidthOut = kpSelection::preferredMinimumWidthForTextStyle (textStyle);
+    
+    // X increasing?
+    if (m_currentPoint.x () >= m_startPoint.x ())
+    {
+        // Text box extends past document width?
+        if (m_startPoint.x () + *minimumWidthOut - 1 >= document ()->width ())
+        {
+            // Cap width to not extend past but not below smallest possible
+            // selection width.
+            *minimumWidthOut =
+                qMax (kpSelection::minimumWidthForTextStyle (textStyle),
+                    document ()->width () - m_startPoint.x ());
+        }
+    }
+    // X decreasing
+    else
+    {
+        // Text box extends past document start?
+        if (m_startPoint.x () - *minimumWidthOut + 1 < 0)
+        {
+            // Cap width to not extend past but not below smallest possible
+            // selection width.
+            *minimumWidthOut =
+                qMax (kpSelection::minimumWidthForTextStyle (textStyle),
+                    m_startPoint.x () + 1);
+        }
+    }
+
+
+    //
+    // Calculate Height.
+    //
+    
+    // Get reasonable height for a text box.
+    *minimumHeightOut = kpSelection::preferredMinimumHeightForTextStyle (textStyle);
+
+    // Y increasing?
+    if (m_currentPoint.y () >= m_startPoint.y ())
+    {
+        // Text box extends past document height?
+        if (m_startPoint.y () + *minimumHeightOut - 1 >= document ()->height ())
+        {
+            // Cap height to not extend past but not below smallest possible
+            // selection height.
+            *minimumHeightOut =
+                qMax (kpSelection::minimumHeightForTextStyle (textStyle),
+                    document ()->height () - m_startPoint.y ());
+        }
+    }
+    // Y decreasing
+    else
+    {
+        // Text box extends past document start?
+        if (m_startPoint.y () - *minimumHeightOut + 1 < 0)
+        {
+            // Cap width to not extend past but not below smallest possible
+            // selection h.
+            *minimumHeightOut =
+                qMax (kpSelection::minimumHeightForTextStyle (textStyle),
+                    m_startPoint.y () + 1);
+        }
+    }
+
+
+    return true/*do create text box*/;
+}
+
 // protected virtual [base kpToolSelection]
 void kpToolText::createMoreSelectionAndUpdateStatusBar (QPoint thisPoint,
         QRect normalizedRect)
 {
     const kpTextStyle textStyle = mainWindow ()->textStyle ();
 
-    int minimumWidth, minimumHeight;
-
-    // Just a click?
-    if (!m_dragHasBegun && thisPoint == m_startPoint)
-    {
-    #if DEBUG_KP_TOOL_TEXT && 1
-        kDebug () << "\tclick creating text box" << endl;
-    #endif
-
-        // (Click creating text box with RMB would not be obvious
-        //  since RMB menu most likely hides text box immediately
-        //  afterwards)
-        if (m_mouseButton == 1)
-            return;
-
-
-        minimumWidth = kpSelection::preferredMinimumWidthForTextStyle (textStyle);
-        if (thisPoint.x () >= m_startPoint.x ())
-        {
-            if (m_startPoint.x () + minimumWidth - 1 >= document ()->width ())
-            {
-                minimumWidth =
-                    qMax (kpSelection::minimumWidthForTextStyle (textStyle),
-                        document ()->width () - m_startPoint.x ());
-            }
-        }
-        else
-        {
-            if (m_startPoint.x () - minimumWidth + 1 < 0)
-            {
-                minimumWidth =
-                    qMax (kpSelection::minimumWidthForTextStyle (textStyle),
-                        m_startPoint.x () + 1);
-            }
-        }
-
-        minimumHeight = kpSelection::preferredMinimumHeightForTextStyle (textStyle);
-        if (thisPoint.y () >= m_startPoint.y ())
-        {
-            if (m_startPoint.y () + minimumHeight - 1 >= document ()->height ())
-            {
-                minimumHeight =
-                    qMax (kpSelection::minimumHeightForTextStyle (textStyle),
-                        document ()->height () - m_startPoint.y ());
-            }
-        }
-        else
-        {
-            if (m_startPoint.y () - minimumHeight + 1 < 0)
-            {
-                minimumHeight =
-                    qMax (kpSelection::minimumHeightForTextStyle (textStyle),
-                        m_startPoint.y () + 1);
-            }
-        }
-    }
-    else
-    {
-    #if DEBUG_KP_TOOL_TEXT && 1
-        kDebug () << "\tdrag creating text box" << endl;
-    #endif
-        minimumWidth = kpSelection::minimumWidthForTextStyle (textStyle);
-        minimumHeight = kpSelection::minimumHeightForTextStyle (textStyle);
-    }
+    // (will set both variables)
+    int minimumWidth = 0, minimumHeight = 0;
+    if (!shouldCreate (textStyle, &minimumWidth, &minimumHeight))
+        return;
 
 
     if (normalizedRect.width () < minimumWidth)

tools/selection/kptooltext.h
@@ -114,6 +114,10 @@ public:
 protected:
     virtual kpToolSelection::DragType beginDrawInsideSelection ();
     virtual QCursor cursorInsideSelection () const;
+private:
+    bool shouldCreate (const kpTextStyle &textStyle,
+        int *minimumWidthOut, int *minimumHeightOut);
+protected:
     virtual void createMoreSelectionAndUpdateStatusBar (QPoint thisPoint,
         QRect normalizedRect);
     virtual void setSelectionBorderForHaventBegunDraw ();

commit dc5822ccec05349f2306c8cf0abd686d2e479a8d

Restore accidental drag detection for text box creation: * Use "accidentalDragAdjustedPoint" (formerly called "thisPoint") instead of "currentPoint()".
Broken by:

r606478 | dang | 2006-11-21 02:08:22 +1100 (Tue, 21 Nov 2006) | 2 lines
Changed paths:
   M /trunk/KDE/kdegraphics/kolourpaint/tools/selection/kptooltext.cpp
   M /trunk/KDE/kdegraphics/kolourpaint/tools/selection/kptooltext.h

Split some code out of kpToolText::createMoreSelectionAndUpdateStatusBar() into shouldCreate()



svn path=/trunk/KDE/kdegraphics/kolourpaint/; revision=607890

tools/selection/kptooltext.cpp
@@ -275,10 +275,11 @@ int kpToolText::calcClickCreateDimension (int mouseStart, int mouseEnd,
 }
 
 // private
-bool kpToolText::shouldCreate (const kpTextStyle &textStyle,
+bool kpToolText::shouldCreate (const QPoint &accidentalDragAdjustedPoint,
+        const kpTextStyle &textStyle,
         int *minimumWidthOut, int *minimumHeightOut)
 {
-    if (m_dragHasBegun || currentPoint () != startPoint ())
+    if (m_dragHasBegun || accidentalDragAdjustedPoint != startPoint ())
     {
     #if DEBUG_KP_TOOL_TEXT && 1
         kDebug () << "\tdrag creating text box" << endl;
@@ -313,7 +314,7 @@ bool kpToolText::shouldCreate (const kpTextStyle &textStyle,
     // Calculate suggested width.
     *minimumWidthOut = calcClickCreateDimension (
         startPoint ().x (),
-            currentPoint ().x (),
+            accidentalDragAdjustedPoint.x (),
         kpSelection::preferredMinimumWidthForTextStyle (textStyle),
             kpSelection::minimumWidthForTextStyle (textStyle),
         document ()->width ());
@@ -321,7 +322,7 @@ bool kpToolText::shouldCreate (const kpTextStyle &textStyle,
     // Calculate suggested height.
     *minimumHeightOut = calcClickCreateDimension (
         startPoint ().y (),
-            currentPoint ().y (),
+            accidentalDragAdjustedPoint.y (),
         kpSelection::preferredMinimumHeightForTextStyle (textStyle),
             kpSelection::minimumHeightForTextStyle (textStyle),
         document ()->height ());
@@ -347,8 +348,11 @@ void kpToolText::createMoreSelectionAndUpdateStatusBar (
 
     // (will set both variables)
     int minimumWidth = 0, minimumHeight = 0;
-    if (!shouldCreate (textStyle, &minimumWidth, &minimumHeight))
+    if (!shouldCreate (accidentalDragAdjustedPoint, textStyle,
+            &minimumWidth, &minimumHeight))
+    {
         return;
+    }
 
 
     // Make sure the dragged out rectangle is of the minimum width we just

tools/selection/kptooltext.h
@@ -127,7 +127,8 @@ private:
     int calcClickCreateDimension (int mouseStart, int mouseEnd,
         int preferredMin, int smallestMin,
         int docSize);
-    bool shouldCreate (const kpTextStyle &textStyle,
+    bool shouldCreate (const QPoint &accidentalDragAdjustedPoint,
+        const kpTextStyle &textStyle,
         int *minimumWidthOut, int *minimumHeightOut);
 protected:
     virtual void createMoreSelectionAndUpdateStatusBar (