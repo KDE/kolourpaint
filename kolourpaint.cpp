81d38d69c6c73473a4867defbff79e54b08334ad
Martin Koller <kollix@aon.at>
2016-09-02 09:51:38 +0200

    Fix handling of commandline filename argument when given relative path
    When a relative path was given as argument, an invalid QUrl was used,
    leading to not opening the file. Fixed that.
    Also when a non-existing filename was given, we now detect the mime-type
    by the filename extension so that saving this file will correctly do so.
    
    BUG: 368131
    FIXED-IN: 16.08.1

--- kolourpaint.cpp
+++ kolourpaint.cpp
@@ -1,6 +1,6 @@
 /*
    Copyright (c) 2003-2007 Clarence Dang <dang@kde.org>
-   Copyright (c) 2015 Martin Koller <kollix@aon.at>
+   Copyright (c) 2015,2016 Martin Koller <kollix@aon.at>
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
