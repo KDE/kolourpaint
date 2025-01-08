60d0098ec790b5f01d39d0688935be6218686e26
Nicolas Fella <nicolas.fella@gmx.de>
2025-01-09 00:17:26 +0100

    Convert license statements to SPDX

--- imagelib/effects/blitz.cpp
+++ imagelib/effects/blitz.cpp
@@ -3,32 +3,13 @@
 /*
  Copyright (C) 1998, 1999, 2001, 2002, 2004, 2005, 2007
       Daniel M. Duley <daniel.duley@verizon.net>
- (C) 2004 Zack Rusin <zack@kde.org>
- (C) 2000 Josef Weidendorfer <weidendo@in.tum.de>
- (C) 1999 Geert Jansen <g.t.jansen@stud.tue.nl>
- (C) 1998, 1999 Christian Tibirna <ctibirna@total.net>
- (C) 1998, 1999 Dirk Mueller <mueller@kde.org>
+ SPDX-FileCopyrightText: 2004 Zack Rusin <zack@kde.org>
+ SPDX-FileCopyrightText: 2000 Josef Weidendorfer <weidendo@in.tum.de>
+ SPDX-FileCopyrightText: 1999 Geert Jansen <g.t.jansen@stud.tue.nl>
+ SPDX-FileCopyrightText: 1998, 1999 Christian Tibirna <ctibirna@total.net>
+ SPDX-FileCopyrightText: 1998, 1999 Dirk Mueller <mueller@kde.org>
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+SPDX-License-Identifier: BSD-2-Clause
 
 */
 
@@ -36,7 +17,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  Portions of this software were originally based on ImageMagick's
  algorithms. ImageMagick is copyrighted under the following conditions:
 
-Copyright (C) 2003 ImageMagick Studio, a non-profit organization dedicated to
+Copyright (C) 2003 ImageMagick Studio a non-profit organization dedicated to
 making software imaging solutions freely available.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
--- lgpl/generic/kpColorCollection.cpp
+++ lgpl/generic/kpColorCollection.cpp
@@ -3,23 +3,10 @@
 // HITODO: Test when remote file support in KDE 4 stabilizes
 
 /* This file is part of the KDE libraries
-    Copyright (C) 1999 Waldo Bastian (bastian@kde.org)
-    Copyright (C) 2007 Clarence Dang (dang@kde.org)
+    SPDX-FileCopyrightText: 1999 Waldo Bastian (bastian@kde.org)
+    SPDX-FileCopyrightText: 2007 Clarence Dang (dang@kde.org)
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+    SPDX-License-Identifier: LGPL-2.0-only
 */
 //-----------------------------------------------------------------------------
 // KDE color collection
